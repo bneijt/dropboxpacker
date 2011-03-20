505e6d76fa07b783abea9da223b7532ee1d796bf
A. Bram Neijt <bneijt@gmail.com>
2011-03-20 22:46:33 +0100

    Up version

--- dropboxpacker.rb
+++ dropboxpacker.rb
@@ -26,7 +26,7 @@ HOSTPATH=File.join(Etc.getpwuid.dir, 'Videos')
 DROPPATH=File.join(Etc.getpwuid.dir, 'Dropbox', 'dropboxpacker')
 LISTFILE=File.join(DROPPATH, 'list.txt')
 MAX_SIZE=Integer(1.5 * 1024 * 1024 * 1024) #1.5GB
-PROGRAM_VERSION='0.0.1'
+PROGRAM_VERSION='0.0.2'
 
 #Load and update the list of files
 def loadListFile()
